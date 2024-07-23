4988ceffda63d0d166f5d16aa501fabf993e2620
Amit Kumar <15000amitkumar@gmail.com>
2024-07-23 17:44:48 +0530

    Rating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,102 @@
 import { MaxWidthWrapper } from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-slate-50">
       <section>
-        <MaxWidthWrapper>
-          <div>
-            <div>
-              <div>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52 ">
+          <div className="col-span-2 px-6 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50  from-slate-50  h-28" />
                 <Image
+                  width={100}
+                  height={100}
                   src="/snake-1.png"
-                  alt="Snake"
-                  width={500}
-                  height={500}
+                  alt="Cobra"
+                  className="w-full"
                 />
               </div>
-              <h1>
-                Your Image on a <span>Custom</span> Phone Case
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case
               </h1>
-              <p>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Transform your favorite memories into unique art with our{" "}
-                <span>one-of-a-kind</span> phone cases. With Case-Cobra, you
-                protect not just your phone, but the moments that matter most.
+                <span className="font-semibold">one-of-a-kind</span> phone
+                cases. With Case-Cobra, you protect not just your phone, but the
+                moments that matter most.
               </p>
-              <ul className="flex flex-col font-medium items-center text-left mt-8 space-y-2 sm:items-start">
-                <div>
-                  <li>
-                    <Check className="h-5 w-5 text-green-600" />
-                    High quality, durable material
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    High-quality, durable material
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
+                    print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Modern iPhone models supported
                   </li>
                 </div>
               </ul>
+              <div className="mt-12 flex items-center sm:items-start gap-16">
+                <div className="flex -space-x-4">
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block h-10 w-10  rounded-full ring-2 ring-slate-100"
+                    src="/users/user-4.jpg"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block  h-10 w-10 object-cover rounded-full ring-2 ring-slate-100"
+                    src="/users/user-5.jpg"
+                    alt="user image"
+                  />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250 Lakhs</span> Happy
+                    Customers
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
